df50e63f9d59d9a9c3c5cf23bac5c069060a8ea3
Harry <hoangnv725@gmail.com>
2025-03-30 08:56:18 +0700

    create Enemy

---
+++ Assets/Scripts/Enemies/EnemyController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D rb;
+    private EnemyStats stats;
+
+    private void Awake()
+    {
+        this.stats = GetComponent<EnemyStats>();
+        this.rb = GetComponent<Rigidbody2D>();
+    }
+
+    public void TakeDamage(int damage)
+    {
+        this.stats.health -= damage;
+        Debug.Log(this.stats.health);
+
+        if (stats.health <= 0)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        Debug.Log("Enemy defeated!");
+        Destroy(this.gameObject);
+    }
+}
